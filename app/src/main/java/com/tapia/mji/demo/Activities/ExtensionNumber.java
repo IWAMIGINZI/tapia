b7ec8eb4f997f513fa51ba2d84bc4e1189d37a79
IWAMIGINZI <k.honden@iisnet.co.jp>
2018-01-25 13:02:54 +0900

    commit　20180125 001

---
+++ app/src/main/java/com/tapia/mji/demo/Activities/ExtensionNumber.java
@@ -0,0 +1,23 @@
+package com.tapia.mji.demo.Activities;
+
+/**
+ * Created by ais75114 on 2018/01/23.
+ */
+
+//内線番号を格納するクラス
+public class ExtensionNumber {
+    final String eigyo1="3152";     //第一事業部-営業部
+    final String eigyo2="2206";     //第二事業部-営業部
+    final String eigyo3="6006";     //第三事業部-営業部
+    final String unyo1="1103";      //第一事業部-運用部
+    final String unyo2="3021";      //第二事業部-運用部
+    final String unyo3="3211";      //第三事業部-運用部
+    final String kaihatu1="1101";   //第一事業部-開発部
+    final String kaihatu2="2302";   //第二事業部-開発部
+    final String kaihatu3="3103";   //第三事業部-開発部
+    final String kiban="3111";      //技術本部-基盤ソリューション部
+    final String tokatu="5104";     //技術本部-プロジェクト統轄部
+    final String ict="6021";        //技術本部-ICTソリューション推進部
+    final String kanri="5807";      //管理部
+    final String support="1532";    //サポートデスク
+}
